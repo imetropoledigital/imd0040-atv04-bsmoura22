def4b2a19e47ec48b9ec0eee1be751f000e1f7b6
Beatriz <bia.s.moura9@gmail.com>
2024-04-11 19:38:38 -0300

    att

--- src/main/java/br/ufrn/imd/Gerente.java
+++ src/main/java/br/ufrn/imd/Gerente.java
@@ -3,7 +3,7 @@ package br.ufrn.imd;
 public class Gerente extends Funcionario {
 
     protected enum Nivel{
-        JUNIOR, PLENO, SERIOR;
+        JUNIOR(1000), PLENO(2000), SENIOR(3000);
 
         private double bonus;
 
@@ -12,10 +12,17 @@ public class Gerente extends Funcionario {
         }
 
     }
+    private Nivel nivel;
     Gerente(int chMensal, double hh, Nivel nivel){
      super(chMensal,
              hh);
+     this.nivel = nivel;
 
-
+    }
+    @Override
+    public double calcularSalario(){
+        double SalBase = chMensal*hh;
+        double SalFinal = SalBase+ nivel.bonus;
+        return SalFinal;
     }
 }
